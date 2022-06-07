13754e988220873e1bb2cdd01ffdcec7aee83c43
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-06-07 14:36:31 +0200

    Initial spigot commit

---
+++ src/main/java/me/kx/redscript/RedScript.java
@@ -0,0 +1,17 @@
+package me.kx.redscript;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RedScript extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
