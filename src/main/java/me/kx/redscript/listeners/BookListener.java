f14a83a048085b6170146fbcef6c9c7d94f60689
RedstoneFuture <redstonefuture@redstoneworld.de>
2023-03-13 17:52:58 +0100

    Reworking event listener

--- src/main/java/me/kx/redscript/listeners/BookListener.java
+++ src/main/java/me/kx/redscript/listeners/BookListener.java
@@ -1,5 +1,6 @@
 package me.kx.redscript.listeners;
 
+import me.kx.redscript.RedScript;
 import me.kx.redscript.utils.BulkCommandExecutor;
 import me.kx.redscript.utils.ParserUtil;
 import org.bukkit.Material;
@@ -7,30 +8,52 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 
 public class BookListener implements Listener {
 
+	//anti-spam
+	boolean PlayerInteractCancel = false;
+	
+	
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent e) {
-		if (!e.getPlayer().isSneaking()) {
-			return;
-		} else if (e.getAction() != Action.LEFT_CLICK_AIR && e.getAction() != Action.LEFT_CLICK_BLOCK) {
-			return;
-		} else if (e.getItem() == null) {
-			return;
-		} else if (e.getItem().getType() != Material.WRITABLE_BOOK && e.getItem().getType() != Material.BOOK) {
-			return;
-		}
 
-		List<String> commands;
-		try {
-			commands = ParserUtil.parseCommandsFromPages(e.getItem().getItemMeta());
-		} catch (Exception ignored) {
-			return;
+		if (PlayerInteractCancel) return;
+
+		// if (!e.getPlayer().isSneaking()) return;
+
+		if (e.getItem() == null) return;
+		if ((e.getItem().getType() != Material.WRITABLE_BOOK) && (e.getItem().getType() != Material.WRITTEN_BOOK)) return;
+
+		
+		if ((e.getAction() == Action.LEFT_CLICK_AIR) || (e.getAction() == Action.LEFT_CLICK_BLOCK)) {
+			
+			List<String> commands;
+			try {
+				commands = ParserUtil.parseCommandsFromPages(e.getItem().getItemMeta());
+			} catch (Exception ignored) {
+				return;
+			}
+
+			BulkCommandExecutor.executeCommandsAsync(e.getPlayer(), commands);
+
+			e.setCancelled(true);
 		}
-		e.setCancelled(true);
-		BulkCommandExecutor.executeCommandsAsync(e.getPlayer(), commands);
+		
+		
+		// anti-spam task
+		PlayerInteractCancel = true;
+
+		new BukkitRunnable() {
+			public void run() {
+
+				PlayerInteractCancel = false;
+
+			}
+		}.runTaskLater(RedScript.getInstance(), 15);
+		
 	}
 }
